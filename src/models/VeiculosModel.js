1f4617f659007ee3ae1fb1ee8804d427130af398
Victor Vechi <victorhvechi@gmail.com>
2023-10-30 18:25:22 -0300

    cria o model para o veículo

---
+++ src/models/VeiculosModel.js
@@ -0,0 +1,14 @@
+class VeiculosModel{
+    constructor(locadora, modelo, marca, ano, motor, portas, cambio, ar_condicionado, updatedAt, createdAt){
+        this.locadora = locadora
+        this.modelo = modelo
+        this.marca = marca
+        this.ano = ano
+        this.motor = motor
+        this.portas = portas
+        this.cambio = cambio
+        this.ar_condicionado = ar_condicionado
+        this.updatedAt = updatedAt
+        this.createdAt = createdAt
+    }
+} export default Veiculo
