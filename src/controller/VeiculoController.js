71b99db33493ae13b512faabf647179dea1fcad7
Victor Vechi <victorhvechi@gmail.com>
2023-10-30 16:50:32 -0300

    cria a base das rotas a serem implementadas

---
+++ src/controller/VeiculoController.js
@@ -0,0 +1,38 @@
+
+
+class VeiculoController {
+
+    /**
+     * @param {Express} app
+     */
+
+    static rotas(app){
+
+        app.get('/veiculos', (request, response) => {
+
+
+        })
+
+        app.get('/veiculos/:id', (request, response) => {
+            
+
+        })
+
+        app.post('/veiculos', (request, response) => {
+            
+
+        })
+
+        app.put('/veiculos:id', (request, response) => {
+            
+
+        })
+
+        app.delete('/veiculos:id', (request, response) => {
+            
+
+        })
+    }
+
+
+}
