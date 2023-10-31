003f030b4faad5ae48ffc00ab0b80fc1c355d918
VictorVechi <victorhvechi@gmail.com>
2023-10-30 23:37:15 -0300

    realiza a conexão e sincronização com o database

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 import express from "express";
-import VeiculoController from "./src/controller/VeiculoController";
+import VeiculoController from "./src/controller/VeiculoController.js";
+import sequelize from "./src/config/database.js";
 
-
+// Inicia o express
 const app = express()
 
+//Seleciona uma porta setado no .env ou a porta 3080
 const PORT = process.env.PORT || 3080
 
-app.listen(PORT, ()=>{
-    console.log(`Aplicação online na porta: ${PORT}`)
-})
+try {
+    //Sincroniza as models com as entidades no banco de dados
+    await sequelize.sync()
+    //Inicia o servidor
+    app.listen(PORT, ()=>{
+        console.log(`Aplicação online na porta: ${PORT}`)
+    })
+} catch (error) {
+    console.log(error.message)
+}
 
+//Configura o express para receber dados em json
 app.use(express.json())
 
+//Inicializa as rotas para veículos
 VeiculoController.rotas(app)
