4acee667fa356922ffce0c9300dd84384f53f4fb
VictorVechi <victorhvechi@gmail.com>
2023-10-31 21:11:50 -0300

    implementa a requisição por id

--- src/controller/VeiculosController.js
+++ src/controller/VeiculosController.js
@@ -7,35 +7,45 @@ class VeiculosController {
      * @param {Express} app
      */
 
-    static rotas(app){
+    static rotas(app) {
 
         app.get('/veiculos', async (request, response) => {
             try {
                 const veiculos = await VeiculosModel.findAll()
                 response.status(200).json(veiculos)
-            } catch(error){
-                response.status(400).json({error: true, message:error})
+            } catch (error) {
+                response.status(400).json({ error: true, message: error })
             }
 
         })
 
-        app.get('/veiculos/:id', (request, response) => {
-            
+        app.get('/veiculos/:id', async (request, response) => {
+            try {
+                const id = request.params.id
+                const veiculo = await VeiculosModel.findByPk(id)
+                if (!!veiculo) {
+                    response.status(200).json(veiculo)
+                } else {
+                    response.status(404).json({ error: false, message: "Veículo não encontrado" })
+                }
+            } catch (error) {
+                response.status(502).json({ error: true, message: error })
+            }
 
         })
 
         app.post('/veiculos', (request, response) => {
-            
+
 
         })
 
         app.put('/veiculos:id', (request, response) => {
-            
+
 
         })
 
         app.delete('/veiculos:id', (request, response) => {
-            
+
 
         })
     }
